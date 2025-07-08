5e8647308efd96a9c988a2dfcf1c5404bcd97708
bluemadrid <63188358+bluemadrid@users.noreply.github.com>
2025-07-08 11:45:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(caldera)
 
 # Variable global para almacenar la temperatura
 current_temperature = {
